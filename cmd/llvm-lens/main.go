01bbf5b91e2cc5ff8552614478d73a1368931769
0xn4utilus <gyanendrabanjare8@gmail.com>
2026-08-29 19:45:41 +0530

    new structure

--- cmd/llvm-lens/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-const version = "0.1.0"
-
-func main() {
-	fmt.Println("llvm-lens", version)
-}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/manasghandat/LLVM-Lens
-
-go 1.24.6
